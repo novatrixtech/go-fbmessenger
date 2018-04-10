7404343b065e5a420af387b784728f3a7e545bb8
Jeff Prestes <jeffprestes@gmail.com>
2018-04-10 17:36:24 +0200

    Add support to message type mandatory field

--- fbmodelsend/letter.go
+++ fbmodelsend/letter.go
@@ -8,14 +8,16 @@ able to find the reciever (or recipient).
 In this case our mail company is Facebook
 */
 type Letter struct {
-	Recipient Recipient `json:"recipient"`
-	Message   Message   `json:"message"`
+	MessageType string    `json:"message_type"`
+	Recipient   Recipient `json:"recipient"`
+	Message     Message   `json:"message"`
 }
 
 /*
 SharedInvite represents a shared button with content where the sender wants to share with a recipient an invite
 */
 type SharedInvite struct {
-	Recipient Recipient                `json:"recipient"`
-	Message   MessageWithSharedContent `json:"message"`
+	MessageType string                   `json:"message_type"`
+	Recipient   Recipient                `json:"recipient"`
+	Message     MessageWithSharedContent `json:"message"`
 }
--- fbmodelsend/senderaction.go
+++ fbmodelsend/senderaction.go
@@ -5,6 +5,7 @@ SenderAction is a struct that represents message states typing_on, typing_off, m
 More details at https://developers.facebook.com/docs/messenger-platform/send-api-reference
 */
 type SenderAction struct {
+	MessageType       string    `json:"message_type"`
 	Recipient         Recipient `json:"recipient"`
 	SenderActionState string    `json:"sender_action"`
 }
